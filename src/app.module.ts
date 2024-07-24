da5023a00b8003b34b1e8d2143f46d27d5946cd6
Frunk Peng <46289416+Graves451@users.noreply.github.com>
2024-07-23 23:25:43 -0700

    cleaned up old db code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { typeOrmConfig } from './database/typeorm.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [
-    // setup the database access for the rest of the code
-    TypeOrmModule.forRoot(typeOrmConfig)
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
