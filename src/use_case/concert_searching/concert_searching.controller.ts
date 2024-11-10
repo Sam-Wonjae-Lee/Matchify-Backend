ce32c8690f7d764d68d8733b776c629a01d364ca
renzi1 <andy.ren@mail.utoronto.ca>
2024-11-10 17:27:14 -0500

    made concert searching a get

--- src/use_case/concert_searching/concert_searching.controller.ts
+++ src/use_case/concert_searching/concert_searching.controller.ts
@@ -1,17 +1,18 @@
-import { Controller, Get, Body, Post} from '@nestjs/common';
-import { ConcertSearchingDto } from './dto/concert_searching.dto';
+import { Controller, Get, Query, Logger, Body, Post } from '@nestjs/common';
 import { ConcertSearchingService } from './concert_searching.service';
-
 
 @Controller('search_concerts')
 export class ConcertSearchingController {
-    constructor(private readonly concertSearchingService: ConcertSearchingService) {}
+    private readonly logger = new Logger(ConcertSearchingController.name);
 
-    @Post()
-    search_concerts(@Body() concertSearchingDto: ConcertSearchingDto) {
-        return this.concertSearchingService.search_concerts(concertSearchingDto)
-        // return this.concertSearchingService.createThreadTable();
+    constructor(private readonly concertSearchingService: ConcertSearchingService) {
+        this.logger.log('ConcertSearchingController initialized');
+    }
 
+    @Get()
+    search_concerts(@Query() query: any) {
+        this.logger.log(`Received query: ${JSON.stringify(query)}`);
+        return this.concertSearchingService.search_concerts(query);
     }
 }
 
